99672a52e3dd01c9d9fd87d00b9506e21bceb586
Krzysztof Kajda <vuelo.kda@gmail.com>
2013-09-27 12:54:50 +0200

    Added DB update of web sites attributes during checking

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,7 @@ import logging
 logging.basicConfig()
 from apscheduler.scheduler import Scheduler
 from model import URLCheck
+from datetime import datetime
 
 
 class WebCheckScheduler(object):
@@ -13,12 +14,15 @@ class WebCheckScheduler(object):
 
     def main_job(self):		  
         websites = self.dbservice.query_db('SELECT * FROM sites')
-        print "\n\n"
-           
-        for	website in websites:	
+  
+        for	website in websites:	            
             url = website['url']    
-            status = self.url_check.get_url_status(url)		
-            print "Checked " + url + " status: " + status
+            webid = website['id']
+            status_code = self.url_check.get_url_status(url)	
+            last_check = datetime.now().strftime('%Y-%m-%d %H:%M:%S')            
+                        
+            self.dbservice.execute_db('UPDATE sites SET last_check=?, status_code=? WHERE id=?', [last_check, status_code, webid])
+
             print "Will send e-mails...TBD\n"
                   	
     def change_interval(self, interval_seconds):	
@@ -29,4 +33,4 @@ class WebCheckScheduler(object):
         self.sched = Scheduler()		
         self.sched.start()	
         self.sched.add_interval_job(self.main_job, seconds = interval_seconds)	
-
+        
